65a3f629f2b06e3ea2fef4b4d52febf1b354e8e1
janice <janicejiang.d@gmail.com>
2017-03-13 17:02:11 +0800

    implement company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313070921) do
+ActiveRecord::Schema.define(version: 20170313082414) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170313070921) do
     t.boolean  "is_hidden",        default: true
     t.string   "image"
     t.string   "location"
+    t.string   "company"
   end
 
   create_table "resumes", force: :cascade do |t|
